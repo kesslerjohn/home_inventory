f019220d57acc6af2ba5ce47c1019616d57ea0bb
kesslerjohn <john-kessler@uiowa.edu>
2024-07-07 19:58:08 -0500

    switch back to basic tkinter

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-import customtkinter as ctk
 from Connection import Connection
 from Item import Item
 from database_utils import *
@@ -13,14 +12,12 @@ from database_utils import *
 #    'view': view_item_info
 #}
 
-class App(ctk.CTk):
+class App(tk.Tk):
 
     def __init__(self, *args, **kwargs):
-        ctk.CTk.__init__(self, *args, **kwargs)
-        ctk.set_appearance_mode("system")
-        ctk.set_default_color_theme("green")
-        container = ctk.CTkFrame(self)
-        container.pack(padx = 60, pady = 20, fill="both", expand = True)
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        container.pack(padx = 10, pady = 5, fill="both", expand = True)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -47,36 +44,36 @@ class App(ctk.CTk):
         frame = self.frames[cont]
         frame.tkraise()
 
-class MainPage(ctk.CTkFrame):
+class MainPage(tk.Frame):
     def __init__(self, parent, controller):
-        ctk.CTkFrame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
 
-        label = ctk.CTkLabel(self, text = "QR Inventory", font = ("Roboto", 35))
+        label = tk.Label(self, text = "QR Inventory", font = ("Roboto", 35))
         label.grid(row = 0, column = 4, padx = 10, pady = 10)
 
-        button1 = ctk.CTkButton(self, text ="Create",
+        button1 = tk.Button(self, text ="Create",
         command = lambda : controller.show_frame(Create))
      
         # putting the button in its place by
         # using grid
         button1.grid(row = 1, column = 1, padx = 10, pady = 10)
   
-class Create(ctk.CTkFrame):
+class Create(tk.Frame):
     def __init__(self, parent, controller):
         
-        ctk.CTkFrame.__init__(self, parent)
-        label = ctk.CTkLabel(self, text ="Create", font = ("Roboto", 35))
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text ="Create", font = ("Roboto", 35))
         label.grid(row = 0, column = 4, padx = 10, pady = 10)
   
         self.features = ["Item name", "Units", "Cost per unit", "Quantity", "Weight", "Datasheet"]
 
         for i in range(len(self.features)):
-            info = ctk.CTkEntry(placeholder_text=self.features[i], master=parent)
+            info = tk.Entry(master=parent)
             info.grid(row=i, column = 0, padx = 10, pady = 10)
 
         # button to show frame 2 with text
         # layout2
-        main_button = ctk.CTkButton(self, text ="Main Page",
+        main_button = tk.Button(self, text ="Main Page",
                             command = lambda : controller.show_frame(MainPage))
      
         # putting the button in its place 
